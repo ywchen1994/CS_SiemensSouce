86dae9c77d4f96ca1c53fcc98d4cc735b4646621
ywchen1994 <asps4401asps4401@gmail.com>
2019-08-12 12:28:28 +0800

    From Siemens

---
+++ OPC_UA_VS_CODE_V12/src/SimpleClient/MainForm.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Opc.Ua;
+using Opc.Ua.Client;
+using Siemens.UAClientHelper;
+
+namespace Siemens.OpcUA.SimpleClient
+{
+    public partial class MainForm : Form
+    {
+        #region Construction
+        public MainForm()
+        {
+            InitializeComponent();
+
+            m_Server = new UAClientHelperAPI();
+            m_Server.CertificateValidationNotification += new CertificateValidationEventHandler(m_Server_CertificateEvent);
+        }
+
+        void m_Server_CertificateEvent(CertificateValidator validator, CertificateValidationEventArgs e)
+        {
+            // Accept all certificate -> better ask user
+            e.Accept = true;
+        }
+        #endregion
+
+        #region Fields
+        private UAClientHelperAPI m_Server = null;
+        private Subscription m_Subscription;
+        private Subscription m_SubscriptionBlock;
+        private UInt16 m_NameSpaceIndex = 0;
+        #endregion
+
+        /// <summary>
+        /// Connect to the UA server and read the namespace table.
+        /// The connect is based on the Server URL entered in the Form
+        /// The read of the namespace table is used to detect the namespace index
+        /// of the namespace URI entered in the Form and used for the variables to read
+        /// </summary>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Connect to the server
+            try
+            {
+                // Connect with URL from Server URL text box
+                m_Server.Connect(txtServerUrl.Text, "none", MessageSecurityMode.None, false, "", "");
+
+                // Toggle enable flag of buttons
+                toggleButtons(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connect failed:\n\n" + ex.Message);
+                return;
+            }
+
+            // Read Namespace Table
+            try
+            {
+                List<string> nodesToRead = new List<string>();
+                List<string> results = new List<string>();
+
+                nodesToRead.Add("ns=0;i=" + Variables.Server_NamespaceArray.ToString());
+
+                // Read the namespace array
+                results = m_Server.ReadValues(nodesToRead);
+
+                if (results.Count != 1)
+                {
+                    throw new Exception("Reading namespace table returned unexptected result");
+                }
+
+                // Try to find the namespace URI entered by the user
+                string[] nameSpaceArray = results[0].Split(';');
+                ushort i;
+                for (i = 0; i < nameSpaceArray.Length; i++)
+                {
+                    if (nameSpaceArray[i] == txtNamespaceUri.Text)
+                    {
+                        m_NameSpaceIndex = i;
+                    }
+                }
+
+                // Check if the namespace was found
+                if ( m_NameSpaceIndex == 0 )
+                {
+                    throw new Exception("Namespace " + txtNamespaceUri.Text + " not found in server namespace table");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Reading namespace table failed:\n\n" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Disconnect from the UA server.
+        /// </summary>
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (m_Subscription != null)
+                {
+                    btnMonitor_Click(null, null);
+                }
+
+                if (m_SubscriptionBlock != null)
+                {
+                    btnMonitorBlock_Click(null, null);
+                }
+
+                // Disconnect from Server
+                m_Server.Disconnect();
+
+                // Toggle enable flag of buttons
+                toggleButtons(false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Disconnect failed:\n\n" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Reads the values of the two variables entered in the From.
+        /// The NodeIds used for the Read are constructed from the identifier entered 
+        /// in the Form and the namespace index detected in the connect method
+        /// </summary>
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> nodesToRead = new List<string>();
+                List<string> results = new List<string>();
+
+                // Add the two variable NodeIds to the list of nodes to read
+                // NodeId is constructed from 
+                // - the identifier text in the text box
+                // - the namespace index collected during the server connect
+                nodesToRead.Add(new NodeId(txtIdentifier1.Text, m_NameSpaceIndex).ToString());
+                nodesToRead.Add(new NodeId(txtIdentifier2.Text, m_NameSpaceIndex).ToString());
+
+                // Read the values
+                results = m_Server.ReadValues(nodesToRead);
+
+                txtRead1.Text = results[0];
+                txtRead2.Text = results[1];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Read failed:\n\n" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Starts the monitoring of the values of the two variables entered in the From.
+        /// The NodeIds used for the monitoring are constructed from the identifier entered 
+        /// in the Form and the namespace index detected in the connect method
+        /// </summary>
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+            // Check if we have a subscription 
+            //  - No  -> Create a new subscription and create monitored items
+            //  - Yes -> Delete Subcription
+            if (m_Subscription == null)
+            {
+                try
+                {
+                    // Create subscription
+                    m_Subscription = m_Server.Subscribe(1000);
+                    m_Server.ItemChangedNotification += new MonitoredItemNotificationEventHandler(ClientApi_ValueChanged);
+                    btnMonitor.Text = "Stop";
+
+                    // Create first monitored item
+                    m_Server.AddMonitoredItem(m_Subscription, new NodeId(txtIdentifier1.Text, m_NameSpaceIndex).ToString(), "item1", 100);
+
+
+                    // Create second monitored item
+                    m_Server.AddMonitoredItem(m_Subscription, new NodeId(txtIdentifier2.Text, m_NameSpaceIndex).ToString(), "item2", 100);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Establishing data monitoring failed:\n\n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    m_Server.RemoveSubscription(m_Subscription);
+                    m_Subscription = null;
+
+                    btnMonitor.Text = "Monitor";
+                    txtMonitored1.Text = "";
+                    txtMonitored2.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Stopping data monitoring failed:\n\n" + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Callback method for data changes from the monitored variables.
+        /// The text boxes for the output of the values or status information are passed 
+        /// to the client API as clientHandles and contained in the callback
+        /// </summary>
+        private void ClientApi_ValueChanged(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke(new MonitoredItemNotificationEventHandler(ClientApi_ValueChanged), monitoredItem, e);
+                    return;
+                }
+                MonitoredItemNotification notification = e.NotificationValue as MonitoredItemNotification;
+                if (notification == null)
+                {
+                    return;
+                }
+
+                if (monitoredItem.DisplayName == "item1")
+                {
+                    // Get the according item
+                    txtMonitored1.Text = notification.Value.WrappedValue.ToString();
+                }
+
+                if (monitoredItem.DisplayName == "item2")
+                {
+                    // Get the according item
+                    txtMonitored2.Text = notification.Value.WrappedValue.ToString();
+                }
+
+                if (monitoredItem.DisplayName == "itemBlock1")
+                {
+                    // Get the according item
+                    txtReadBlock.Text = notification.Value.WrappedValue.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error in the data change callback:\n\n" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Write the value of the first variable entered in the From.
+        /// The NodeId used for the Write is constructed from the identifier entered 
+        /// in the Form and the namespace index detected in the connect method
+        /// </summary>
+        private void btnWrite1_Click(object sender, EventArgs e)
+        {
+            writeNewValue(
+                new NodeId(txtIdentifier1.Text, m_NameSpaceIndex),  // NodeId = identifier + namespace index
+                txtWrite1.Text); // Value to write as string
+        }
+
+        /// <summary>
+        /// Write the value of the second variable entered in the From.
+        /// The NodeId used for the Write is constructed from the identifier entered 
+        /// in the Form and the namespace index detected in the connect method
+        /// </summary>
+        private void btnWrite2_Click(object sender, EventArgs e)
+        {
+            writeNewValue(
+                new NodeId(txtIdentifier2.Text, m_NameSpaceIndex), // NodeId = identifier + namespace index
+                txtWrite2.Text); // Value to write as string
+        }
+
+        /// <summary>
+        /// Helper function to writing a value to a variable.
+        /// The function 
+        /// - reads the data type of the variable
+        /// - converts the passed string to the data type
+        /// - writes the value to the variable
+        /// </summary>
+        private void writeNewValue(NodeId nodeToWrite, string valueToWrite)
+        {
+            try
+            {
+                List<string> nodesToWrite = new List<string>();
+                List<string> values = new List<string>();
+
+                nodesToWrite.Add(nodeToWrite.ToString());
+                values.Add(valueToWrite);
+
+                m_Server.WriteValues(values, nodesToWrite);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Writing new value failed:\n\n" + ex.Message);
+            }
+        }
+
+        private void btnMonitorBlock_Click(object sender, EventArgs e)
+        {
+            // Check if we have a subscription 
+            //  - No  -> Create a new subscription and create monitored item
+            //  - Yes -> Delete Subcription
+            if (m_SubscriptionBlock == null)
+            {
+                try
+                {
+                    // Create subscription
+                    m_SubscriptionBlock = m_Server.Subscribe(1000);
+                    btnMonitorBlock.Text = "Stop";
+
+                    // Create first monitored item
+                    m_Server.AddMonitoredItem(m_SubscriptionBlock, new NodeId(txtIdentifierBlockRead.Text, m_NameSpaceIndex).ToString(), "itemBlock1", 100);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Establishing block monitoring failed:\n\n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    m_Server.RemoveSubscription(m_SubscriptionBlock);
+                    m_SubscriptionBlock = null;
+
+                    btnMonitorBlock.Text = "Monitor Block";
+                    txtReadBlock.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Stopping block monitoring failed:\n\n" + ex.Message);
+                }
+            }
+        }
+
+        private void btnWriteBlock1_Click(object sender, EventArgs e)
+        {
+            int writeLength = (int)Convert.ChangeType(txtWriteLength.Text, typeof(int));
+            byte[] rawValue = new byte[writeLength];
+            byte currentValue = 0;
+            object writeValue;
+
+            for (int i = 0; i < rawValue.Count(); i++)
+            {
+                rawValue[i] = currentValue;
+                currentValue++;
+            }
+
+            writeValue = rawValue;
+
+            writeNewBlockValue(
+                new NodeId(txtIdentifierBlockWrite.Text, m_NameSpaceIndex), // NodeId = identifier + namespace index
+                writeValue); // Value to write as byte array
+        }
+
+        private void btnWriteBlock2_Click(object sender, EventArgs e)
+        {
+            int writeLength = (int)Convert.ChangeType(txtWriteLength.Text, typeof(int));
+            byte[] rawValue = new byte[writeLength];
+            byte currentValue = 255;
+            object writeValue;
+
+            for (int i = 0; i < rawValue.Count(); i++)
+            {
+                rawValue[i] = currentValue;
+                currentValue--;
+            }
+
+            writeValue = rawValue;
+
+            writeNewBlockValue(
+                new NodeId(txtIdentifierBlockWrite.Text, m_NameSpaceIndex), // NodeId = identifier + namespace index
+                writeValue); // Value to write as byte array
+        }
+
+        /// <summary>
+        /// Helper function to writing a value to a variable.
+        /// The function 
+        /// - reads the data type of the variable
+        /// - converts the passed string to the data type
+        /// - writes the value to the variable
+        /// </summary>
+        private void writeNewBlockValue(NodeId nodeToWrite, object valueToWrite)
+        {
+            try
+            {
+                List<string> nodesToWrite = new List<string>();
+                List<string> values = new List<string>();
+
+                nodesToWrite.Add(nodeToWrite.ToString());
+                values.Add(valueToWrite.ToString());
+
+                m_Server.WriteValues(values, nodesToWrite);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Writing new block value failed:\n\n" + ex.Message);
+            }
+        }
+
+        private void toggleButtons(bool isConnected)
+        {
+            // Toggle Connect / Disconnect buttons
+            btnConnect.Enabled = !isConnected;
+            btnDisconnect.Enabled = isConnected;
+
+            // Toggle Textboxes
+            txtServerUrl.Enabled = !isConnected;
+            txtNamespaceUri.Enabled = !isConnected;
+
+            // Toggle action buttons
+            btnMonitor.Enabled = isConnected;
+            btnRead.Enabled = isConnected;
+            btnWrite1.Enabled = isConnected;
+            btnWrite2.Enabled = isConnected;
+            btnMonitorBlock.Enabled = isConnected;
+            btnWriteBlock1.Enabled = isConnected;
+            btnWriteBlock2.Enabled = isConnected;
+        }
+    }
+}
---
+++ OPC_UA_VS_CODE_V12/src/UAClient/WriteValuesDialog.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Siemens.OpcUA.Client
+{
+    partial class WriteValuesDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.WriteValueCH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NodeIdCH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CurrentValueCH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStrip();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.toolStripStatusLabel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(341, 4);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(75, 23);
+            this.buttonApply.TabIndex = 2;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(179, 4);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 3;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(260, 3);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 24);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.WriteValueCH,
+            this.NodeIdCH,
+            this.CurrentValueCH});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.LabelEdit = true;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(419, 210);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // WriteValueCH
+            // 
+            this.WriteValueCH.Text = "Write Value";
+            this.WriteValueCH.Width = 87;
+            // 
+            // NodeIdCH
+            // 
+            this.NodeIdCH.Text = "NodeId";
+            // 
+            // CurrentValueCH
+            // 
+            this.CurrentValueCH.Text = "Current Value";
+            this.CurrentValueCH.Width = 115;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.buttonOk);
+            this.panel1.Controls.Add(this.buttonApply);
+            this.panel1.Controls.Add(this.buttonCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 179);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 31);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.toolStripStatusLabel);
+            this.panel2.Location = new System.Drawing.Point(3, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(173, 27);
+            this.panel2.TabIndex = 4;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.LinkVisited = true;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(39, 24);
+            this.toolStripLabel1.Text = "Status";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripStatusLabel.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStripStatusLabel.Location = new System.Drawing.Point(0, 0);
+            this.toolStripStatusLabel.Margin = new System.Windows.Forms.Padding(2);
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(173, 27);
+            this.toolStripStatusLabel.Stretch = true;
+            this.toolStripStatusLabel.TabIndex = 4;
+            this.toolStripStatusLabel.Text = "Status";
+            // 
+            // WriteValuesDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(419, 210);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView);
+            this.MinimumSize = new System.Drawing.Size(290, 38);
+            this.Name = "WriteValuesDialog";
+            this.Text = "Write Values";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.toolStripStatusLabel.ResumeLayout(false);
+            this.toolStripStatusLabel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader WriteValueCH;
+        private System.Windows.Forms.ColumnHeader NodeIdCH;
+        private System.Windows.Forms.ColumnHeader CurrentValueCH;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStrip toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+
+
+    }
+}
